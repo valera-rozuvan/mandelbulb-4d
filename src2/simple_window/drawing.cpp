a3b9ee89a5ed3f3046204bcaa3e8a6a9c52f7a0c
Valera Rozuvan <valera.rozuvan@gmail.com>
2017-07-06 15:37:37 +0300

    Trying to draw texture inside window

--- src2/simple_window/drawing.cpp
+++ src2/simple_window/drawing.cpp
@@ -1,5 +1,5 @@
 struct nk_command_buffer *canvas;
-const struct nk_color grid_color = nk_rgb(255, 255, 255);
+const struct nk_color grid_color = nk_rgba(255, 255, 255, 255);
 struct nk_vec2 upperCoord;
 
 const unsigned int WIDTH_IMG = 80;
@@ -7,9 +7,13 @@ const unsigned int HEIGHT_IMG = 80;
 
 unsigned char imageData[WIDTH_IMG * HEIGHT_IMG * 4];
 
+struct nk_image myImage;
+struct nk_rect total_space;
+
 void draw_line(struct nk_context* ctx)
 {
   canvas = nk_window_get_canvas(ctx);
+  total_space = nk_window_get_content_region(ctx);
 
   // Draw simple line.
   upperCoord = nk_window_get_content_region_min(ctx);
@@ -17,19 +21,19 @@ void draw_line(struct nk_context* ctx)
 
   // Create an image. For now all pixels are the same.
   int i = 0;
-  for(unsigned int y = 0; y < HEIGHT_IMG; ++y){
-      for(unsigned int x = 0; x < WIDTH_IMG; ++x){
-        imageData[i] = 174;
-        imageData[i + 1] = 79;
-        imageData[i + 2] = 206;
-        imageData[i + 3] = 255;
+  for (unsigned int y = 0; y < HEIGHT_IMG; ++y){
+    for (unsigned int x = 0; x < WIDTH_IMG; ++x){
+      imageData[i] = 174;
+      imageData[i + 1] = 79;
+      imageData[i + 2] = 206;
+      imageData[i + 3] = 255;
 
-        i += 4;
-     }
+      i += 4;
+    }
   }
 
   // creating a texture
-  unsigned int texture = 0;
+  unsigned int texture = 42;
   glGenTextures(1, &texture);
   glBindTexture(GL_TEXTURE_2D, texture);
 
@@ -42,9 +46,10 @@ void draw_line(struct nk_context* ctx)
   // allocate memory and set texture data
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, WIDTH_IMG, HEIGHT_IMG, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
 
-  // TODO: How to render texture inside current nuklear window?
+  // render texture inside current nuklear window
+  myImage = nk_image_id((int)texture);
+  nk_draw_image(canvas, total_space, &myImage, grid_color);
 
   // delete texture
   glDeleteTextures(1, &texture);
-
 }
